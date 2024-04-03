8037b7626a0c206b1680c1c7e91781ba1b77bdd7
Emily Surface <es00028@mix.wvu.edu>
2024-04-03 11:38:25 -0400

    DresstoImpress and Appsettings.json

---
+++ DressToImpress/Pages/FashionCRUD/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DresstoImpress.Data;
+using DresstoImpressAPI2.Entities;
+
+namespace DresstoImpress.Pages.FashionCRUD
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DresstoImpress.Data.ApplicationDbContext _context;
+
+        public DetailsModel(DresstoImpress.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Clothing Clothing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clothing = await _context.Clothing.FirstOrDefaultAsync(m => m.ClothingID == id);
+            if (clothing == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Clothing = clothing;
+            }
+            return Page();
+        }
+    }
+}

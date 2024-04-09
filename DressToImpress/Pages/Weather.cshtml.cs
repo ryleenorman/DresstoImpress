127443220fb574965cb2b2cde08bbecc2c35c77f
ryleenorman <rln00005@mix.wvu.edu>
2024-04-09 15:28:21 -0400

    Add Weather Dynamic Page

---
+++ DressToImpress/Pages/Weather.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DresstoImpress.Pages
+{
+    public class WeatherModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
